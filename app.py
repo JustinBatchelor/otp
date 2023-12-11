4313d99492993e22033c080f30779c9f1b53bc5f
Justin Batchelor <justinrossbatchelor@gmail.com>
2023-12-10 22:50:41 -0500

    updating code

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, jsonify
+import pyotp
+import hashlib
+import os
+
+app = Flask(__name__)
+
+def generate_totp_sha256(secret):
+    """
+    Generate a TOTP code based on the given secret using SHA256 algorithm.
+
+    Args:
+    secret (str): The secret key for the TOTP generation.
+
+    Returns:
+    str: The TOTP code.
+    """
+    totp = pyotp.TOTP(secret, digest=hashlib.sha256)
+    return totp.now()
+
+@app.route('/otp/get', methods=['GET'])
+def get_otp():
+    # For demo purposes, using a fixed secret key. In a real application, 
+    # this should be dynamically retrieved from a secure store or user input.
+    print(os.environ.get("OTPSECRET"))
+    otp_code = generate_totp_sha256(os.environ.get("OTPSECRET"))
+    return jsonify({'otp': otp_code})
+
+if __name__ == '__main__':
+    app.run(debug=True)
